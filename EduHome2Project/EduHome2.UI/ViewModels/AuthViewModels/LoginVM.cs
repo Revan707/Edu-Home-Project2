01b57ea09a20bbfefa4207d4d5a7498fc31d8641
Revan707 <ravangh@code.edu.az>
2023-07-11 12:01:00 +0400

    LoginPage is done

--- EduHome2Project/EduHome2.UI/ViewModels/AuthViewModels/LoginVM.cs
+++ EduHome2Project/EduHome2.UI/ViewModels/AuthViewModels/LoginVM.cs
@@ -1,6 +1,12 @@
-﻿namespace EduHome2.UI.ViewModels.AuthViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduHome2.UI.ViewModels.AuthViewModels;
+
+public class LoginVM
 {
-    public class LoginVM
-    {
-    }
+    [Required]
+    public string Username { get; set; } = null!;
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+    public bool RememberMe { get; set; }
 }
